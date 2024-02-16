8048ab964786390f38cd38d631362eb3bb2f2ed0
Tatiana <x780am@gmail.com>
2024-02-16 20:58:29 +0300

    commit

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
         
     ###  Настройка БД  ###
     MYSQL_HOST = os.environ.get('MYSQL_HOST')
